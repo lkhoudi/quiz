10997442f0f2cd4f2bec90270dd9e2a95700604e
lkhoudi <lkhoudi@cosium.com>
2019-05-26 22:06:51 +0200

    refactoring + html ok

--- src/main/java/com/quiz/demoQuiz/web/UserController.java
+++ src/main/java/com/quiz/demoQuiz/web/UserController.java
@@ -11,8 +11,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -62,10 +64,10 @@ public class UserController {
     }
 
     @RequestMapping(value= {"/play"}, method=RequestMethod.POST)
-    public ModelAndView play(@Valid Options options){
+    public ModelAndView play(@RequestParam String themes, @RequestParam String levels){
         ModelAndView modelAndView = new ModelAndView();
         ParserJson parserJson = new ParserJson();
-        Quizz quizz = parserJson.jsonparser(QuizOptions.valueOf(options.getTheme().toUpperCase()).name(), options.getLevel() );
+        Quizz quizz = parserJson.jsonparser(QuizOptions.valueOf(themes).name(), levels);
         modelAndView.addObject("quizz",quizz);
         modelAndView.setViewName("home/play");
         return modelAndView;
